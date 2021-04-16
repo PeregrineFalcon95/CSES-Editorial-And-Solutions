16b0b4d2f46b6f9be054e7481d7b5b6f4a565014
Amit Sarker <amitcse1995@gmail.com>
2021-04-16 06:45:12 +0600

    Create Weird Algorithm.cpp
    Solved Weired Algorithm

---
+++ Introductory Problems/Weird Algorithm.cpp
@@ -0,0 +1,59 @@
+/* Editorial: 
+Run a While loop with a condition that runs until the value of N becomes 1.
+With each iteration, if the value os odd, replace the N with N * 3 + 1.
+Otherwise, replace N with N / 2.
+*/
+
+
+
+
+#include<bits/stdc++.h>
+//#include <ext/pb_ds/tree_policy.hpp>
+//#include <ext/pb_ds/assoc_container.hpp>
+//#pragma GCC optimize("Ofast")
+//#pragma GCC target("avx,avx2,fma")
+//#pragma GCC optimization ("unroll-loops")
+using namespace std;
+//using namespace __gnu_pbds;
+//typedef tree<int,null_type,less<int>,rb_tree_tag,tree_order_statistics_node_update>pbds;
+//#pragma comment(linker, "/stack:200000000")
+#define fast ios_base::sync_with_stdio(NULL);cin.tie(NULL);cout.tie(NULL);
+#define ll long long int
+#define mod ( ll  ) 1000000000000//( 1e9 + 7 )  //( 1e9 + 7 )
+#define inf ( int  )( 1e9 + 10 )
+#define mxs (int)(1e6+100)
+#define CHECK_BIT(var,pos) ((var) & (1LL<<(pos)))
+//#define md  int mid = ( l + r )>>1;
+//int dx[]={+1,-1,+0,+0};///Four Directions
+//int dy[]={+0,+0,+1,-1};///Four directions
+//int dx [] = {+1,-1,+0,+0,-1,-1,+1,+1};///Eight Directions
+//int dy [] = {+0,+0,+1,-1,+1,-1,-1,+1};///Eight Directions
+//int dx[]={-2,-2,-1,1,-1,1,2,2};///Knight moves
+//int dy[]={1,-1,-2,-2,2,2,-1,1};///Knight moves
+
+ll n , m, t, ans, cases ;
+ll ar [ mxs ] ;
+string s , s2;
+void brainfuck();
+int main()
+{
+    //freopen("input.txt","r",stdin);
+    //freopen("output.txt","w",stdout);
+    fast
+    //cin>>t;
+    t = 1;
+    //scanf("%d",&t);
+    while( t-- )brainfuck();
+    return 0;
+}
+void brainfuck()
+{
+    cin>>n;
+    while( n != 1 )
+    {
+        cout<<n<<" ";
+        if ( n&1 )n = n * 3 + 1;
+        else n /= 2;
+    }
+    cout<<n;
+}
