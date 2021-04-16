42ace79547ed515d85a66eae157a194eb788a125
Amit Sarker <amitcse1995@gmail.com>
2021-04-16 09:15:29 +0600

    Create Number Spiral.cpp

---
+++ Introductory Problems/Number Spiral.cpp
@@ -0,0 +1,78 @@
+/* Editorial:
+See the code please. It's hard to explain and easy to understand.
+Time complexity for each query : O(1)
+*/
+
+
+
+
+#include<bits/stdc++.h>
+//#include <ext/pb_ds/tree_policy.hpp>
+//#include <ext/pb_ds/assoc_container.hpp>
+//#pragma GCC optimize("Ofast")
+//#pragma GCC target("avx,avx2,fma")
+//#pragma GCC optimization ("unroll-loops")
+using namespace std;
+//using namespace __gnu_pbds;
+//typedef tree<int,null_type,less<int>,rb_tree_tag,tree_order_statistics_node_update>pbds;
+//#pragma comment(linker, "/stack:200000000")
+#define fast ios_base::sync_with_stdio(NULL);cin.tie(NULL);cout.tie(NULL);
+#define ll long long int
+#define mod ( ll  ) 1000000000000//( 1e9 + 7 )  //( 1e9 + 7 )
+#define inf ( int  )( 1e9 + 10 )
+#define mxs (int)(1e6+100)
+#define CHECK_BIT(var,pos) ((var) & (1LL<<(pos)))
+//#define md  int mid = ( l + r )>>1;
+//int dx[]={+1,-1,+0,+0};///Four Directions
+//int dy[]={+0,+0,+1,-1};///Four directions
+//int dx [] = {+1,-1,+0,+0,-1,-1,+1,+1};///Eight Directions
+//int dy [] = {+0,+0,+1,-1,+1,-1,-1,+1};///Eight Directions
+//int dx[]={-2,-2,-1,1,-1,1,2,2};///Knight moves
+//int dy[]={1,-1,-2,-2,2,2,-1,1};///Knight moves
+
+ll n , m, t, ans, cases ;
+ll ar [ mxs ] ;
+string s , s2;
+void brainfuck();
+int main()
+{
+    //freopen("input.txt","r",stdin);
+    //freopen("output.txt","w",stdout);
+    fast
+    cin>>t;
+    //t = 1;
+    //scanf("%d",&t);
+    while( t-- )brainfuck();
+    return 0;
+}
+void brainfuck()
+{
+    cin>>n>>m;
+    ll mx = max( n , m );
+    ll last = ( mx - 1 ) * ( mx - 1 );
+    if ( n > m )
+    {
+        if ( n&1 )
+        {
+            ans = last + m;
+        }
+        else
+        {
+            last = mx * mx;
+            ans = last - (m-1);
+        }
+    }
+    else
+    {
+        if ( m&1 )
+        {
+            last = mx * mx;
+            ans = last - (n-1);
+        }
+        else
+        {
+            ans = last + n;
+        }
+    }
+    cout<<ans<<"\n";
+}
