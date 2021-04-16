d7ea8366ef69d7128f43d549da7fb3142d57a69b
Amit Sarker <amitcse1995@gmail.com>
2021-04-16 06:52:10 +0600

    Create Missing Number.cpp
    Solved Missing Number Cses

---
+++ Introductory Problems/Missing Number.cpp
@@ -0,0 +1,69 @@
+/* Editorial:
+This problem can be solved using the technique of counting sort.
+Have an array sized N. Initially each index equal to 0.
+Take input of N-1 numbers. Lets say the number is a.
+Make ar [ a ] = 1;
+Iterate through the array. Check if any index value is equal to 0.
+Print the index and exit the program.
+*/
+
+
+
+
+#include<bits/stdc++.h>
+//#include <ext/pb_ds/tree_policy.hpp>
+//#include <ext/pb_ds/assoc_container.hpp>
+//#pragma GCC optimize("Ofast")
+//#pragma GCC target("avx,avx2,fma")
+//#pragma GCC optimization ("unroll-loops")
+using namespace std;
+//using namespace __gnu_pbds;
+//typedef tree<int,null_type,less<int>,rb_tree_tag,tree_order_statistics_node_update>pbds;
+//#pragma comment(linker, "/stack:200000000")
+#define fast ios_base::sync_with_stdio(NULL);cin.tie(NULL);cout.tie(NULL);
+#define ll long long int
+#define mod ( ll  ) 1000000000000//( 1e9 + 7 )  //( 1e9 + 7 )
+#define inf ( int  )( 1e9 + 10 )
+#define mxs (int)(1e6+100)
+#define CHECK_BIT(var,pos) ((var) & (1LL<<(pos)))
+//#define md  int mid = ( l + r )>>1;
+//int dx[]={+1,-1,+0,+0};///Four Directions
+//int dy[]={+0,+0,+1,-1};///Four directions
+//int dx [] = {+1,-1,+0,+0,-1,-1,+1,+1};///Eight Directions
+//int dy [] = {+0,+0,+1,-1,+1,-1,-1,+1};///Eight Directions
+//int dx[]={-2,-2,-1,1,-1,1,2,2};///Knight moves
+//int dy[]={1,-1,-2,-2,2,2,-1,1};///Knight moves
+
+int n , m, t, ans, cases ;
+int ar [ mxs ] ;
+string s , s2;
+void brainfuck();
+int main()
+{
+    //freopen("input.txt","r",stdin);
+    //freopen("output.txt","w",stdout);
+    fast
+    //cin>>t;
+    t = 1;
+    //scanf("%d",&t);
+    while( t-- )brainfuck();
+    return 0;
+}
+void brainfuck()
+{
+    int a;
+    cin>>n;
+    for ( int i = 1 ; i < n; i++ )
+    {
+        cin>>a;
+        ar [ a ] = 1;
+    }
+    for ( int i = 1 ; i <= n; i++ )
+    {
+        if ( !ar [ i ] )
+        {
+            cout<<i<<"\n";
+            return;
+        }
+    }
+}
